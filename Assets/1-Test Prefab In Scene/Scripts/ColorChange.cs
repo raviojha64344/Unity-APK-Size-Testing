2494146b35b82b260d222f61f9013548a9ce60bd
Ravi Oza <ravi.oza@digitas.com>
2020-05-18 13:21:33 +0530

    Initial Commit

---
+++ Assets/1-Test Prefab In Scene/Scripts/ColorChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class ColorChange : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerClick");
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("OnMouseUp");
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+}
---
+++ Assets/2-Test Prefab Instantiate/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject goPrefab;
+
+    [SerializeField]
+    private int count = 100;
+
+    void Start()
+    {
+        Spawn(count);   
+    }
+
+    private void Spawn(int count)
+    {
+        float xPos = 0f,zPos = 0f, yPos = 0f;
+        for (int i = 0; i < count; i++)
+        {
+            GameObject go = Instantiate(goPrefab, transform);
+            go.transform.position = new Vector3(xPos, yPos, zPos);
+            xPos++;
+            if (i % 10 == 0)
+            {
+                xPos = 0f;
+                zPos++;
+            }
+            if(zPos == 10)
+            {
+                zPos = 0;
+                yPos++;
+            }
+        }
+    }
+}
